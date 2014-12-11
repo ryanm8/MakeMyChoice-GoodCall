ae86bbd1b3685e0123379fe58dddaf52c270744d
LegendarySikic <chrisvt@vt.edu>
2014-12-10 19:58:56 -0500

    Chat fixed

--- MakeMyChoice/web/websocket.js
+++ MakeMyChoice/web/websocket.js
@@ -3,52 +3,76 @@
  * Copyright © 2014 Chris Hoffman. All rights reserved. * 
  */
 var wsUri = 'ws://' + document.location.host
- + document.location.pathname.substr(0,
- document.location.pathname.indexOf("/faces"))
- + '/websocket';
+        + document.location.pathname.substr(0,
+                document.location.pathname.indexOf("/faces"))
+        + '/websocket';
 console.log(wsUri);
 var websocket = new WebSocket(wsUri);
 var textField = document.getElementById("textField");
-var users = document.getElementById("users");
+//var users = document.getElementById("users");
 var chatlog = document.getElementById("chatlog");
+var canSend = false;
+var sent = false;
 var username;
-websocket.onopen = function(evt) { onOpen(evt); };
-websocket.onmessage = function(evt) { onMessage(evt); };
-websocket.onerror = function(evt) { onError(evt); };
-websocket.onclose = function(evt) { onClose(evt); };
+var pre = document.createElement("p");
+websocket.onopen = function(evt) {
+    onOpen(evt);
+};
+websocket.onmessage = function(evt) {
+    onMessage(evt);
+};
+websocket.onerror = function(evt) {
+    onError(evt);
+};
+websocket.onclose = function(evt) {
+    onClose(evt);
+};
 var output = document.getElementById("output");
 function join(name) {
- username = name;
- websocket.send(username + " joined");
+    username = name;
+    if(!canSend)
+    {
+        websocket.send(username + " has joined the chat.");
+        canSend = true;
+        pre.innerHTML = "";
+    }
 }
 function send_message() {
- websocket.send(username + ": " + textField.value);
- }
+    if (canSend)
+    {
+        websocket.send(username + ": " + textField.value); 
+    }
+    else
+    {
+        if(!sent)
+        {
+            writeToScreen("Please Join Chat First.");
+            sent = true;
+        } 
+    }
+}
 function onOpen() {
- //writeToScreen("CONNECTED");
+    //writeToScreen("CONNECTED");
 }
 function onClose() {
- //writeToScreen("DISCONNECTED");
+    websocket.send(username + " has left the chat.");
+    canSend = false;
+    sent = false;
 }
 function onMessage(evt) {
- writeToScreen("RECEIVED: " + evt.data);
- if (evt.data.indexOf("joined") !== -1) {
- users.innerHTML += evt.data.substring(0, evt.data.indexOf("joined")) + "\n";
- } else {
- chatlog.innerHTML += evt.data + "\n";
- }
-}
-function onError(evt) {
- writeToScreen('<span style="color: red;">ERROR:</span> ' +
-evt.data);
-}
-function disconnect() {
- websocket.close();
+        chatlog.innerHTML = evt.data + "\n" + chatlog.innerHTML;
 }
 
-function writeToScreen(message) {
- var pre = document.createElement("p");
- pre.style.wordWrap = "break-word";
- pre.innerHTML = message;
- output.appendChild(pre);
+function onError(evt) {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' +
+            evt.data);
+}
+function disconnect() {
+    websocket.close();
+}
+
+function writeToScreen(message) {  
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
 }
